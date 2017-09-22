1ed26cd9b4c7df61a2fe49bc1421a30e3449476f
Abdul Wahab <ab.1303@gmail.com>
2017-09-22 17:15:41 +1000

    Routing and Navigation Pages - part 1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,50 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { Router, RouterModule } from '@angular/router'
 
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./common/toastr.service";
-
+import { EventDetailsComponent } from "./events/event-details/event-details.component";
+import { Error404Component } from "./errors/404.component";
+import { NavBarComponent } from "./nav/navbar.component";
+import { appRoutes } from './routes';
+import { CreateEventComponent } from "./events/create-event.component";
+import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
+import { EventListResolver } from "./events/events-list-resolver";
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    NavBarComponent,    
+    Error404Component
   ],
-  providers:[ EventService, ToastrService],
+  providers:[ 
+    EventService,
+     ToastrService,
+     EventRouteActivator,
+     EventListResolver,
+     {
+       provide:'canDeactivateCreateEvent',
+       useValue:checkDirtyState
+     }
+    ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
+
+function checkDirtyState(component:CreateEventComponent){
+  if(component.isDirty)
+    return window.confirm('You have not saved this event. Do you really want to cancel?')
+  return true;
+}
